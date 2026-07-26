16b14ad0ce002f13895b151f21574a50329dbd92
jazilrazi <jazilrazi03@gmail.com>
2026-07-26 16:00:02 +0530

    Average of Numbers

--- Average_of_Numers.py
+++ Average_of_Numers.py
@@ -1,9 +1,13 @@
 # Python Program to Calculate the Average of Numbers in a Given List
 
+#                                   if the valuse is given
 number = [5,64,5,78,5,4,33,5,64,4,]
 large = max(number)
 print("largest number is :",large) 
 
+
+
+#                                  if tha values is not given
 numbers = []
 count = int(input ("how many numbers:"))
 for i in range(count):
